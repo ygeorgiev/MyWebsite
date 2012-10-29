4051e03a6c48a4ff0c57e19de3818124194a088d
Patrick Uiterwijk <puiterwijk@gmail.com>
2012-10-29 13:27:32 +0100

    Admin module added

---
+++ modules/admin/admin.c
@@ -0,0 +1,96 @@
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <limits.h>
+#include <gio/gio.h>
+#include <libsoup/soup.h>
+
+#include <config.h>
+#include <utils.h>
+#include <web_module.h>
+
+int get_interface_version()
+{
+    return WEB_MODULE_INTERFACE_VERSION;
+}
+
+const char *get_name()
+{
+    return "admin";
+}
+
+const int get_version()
+{
+    return 1;
+}
+
+static const char test_html[] =
+    "<html>"
+    "   <head>"
+    "       <title>MyWebsite testing page</title>"
+    "   </head>"
+    "   <body>"
+    "       <h1>MyWebsite - ADMIN</h1>"
+    "       This website is powered by MyWebsite, an Ansi C custom web server.<br />"
+    "       This website is running version: "
+    PACKAGE_VERSION
+    "       <br />"
+    "   </body>"
+    "</html>";
+
+
+static void admin_callback(SoupServer *server,
+                          SoupMessage *msg,
+                          const char *path,
+                          GHashTable *query,
+                          SoupClientContext *client,
+                          gpointer user_data)
+{
+    soup_message_set_status(msg, SOUP_STATUS_OK);
+    soup_message_set_response(msg, "text/html", SOUP_MEMORY_STATIC, test_html, strlen(test_html));
+}
+
+char *admin_auth_callback (SoupAuthDomain *domain,
+                           SoupMessage *msg,
+                           const char *username,
+                           gpointer user_data)
+{
+    if(strcmp(username, "patrick") == 0)
+    {
+        return soup_auth_domain_digest_encode_password("patrick", "MyWebsite Admin", "dublin");
+    }
+    else
+    {
+        return NULL;
+    }
+}
+
+SoupAuthDomain *admin_domain = NULL;
+
+bool deploy(SoupServer *server)
+{
+    admin_domain = soup_auth_domain_digest_new(SOUP_AUTH_DOMAIN_REALM, "MyWebsite Admin",
+                                               SOUP_AUTH_DOMAIN_DIGEST_AUTH_CALLBACK, admin_auth_callback,
+                                               SOUP_AUTH_DOMAIN_DIGEST_AUTH_DATA, NULL,
+                                               SOUP_AUTH_DOMAIN_ADD_PATH, "/admin",
+                                               NULL);
+
+    soup_server_add_auth_domain(server, admin_domain);
+
+    soup_server_add_handler(server,
+                            "/admin",
+                            admin_callback,
+                            NULL,
+                            NULL);
+
+    return true;
+}
+
+void undeploy(SoupServer *server)
+{
+    soup_server_remove_handler(server,
+                              "/admin");
+    soup_server_remove_auth_domain(server, admin_domain);
+}

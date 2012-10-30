753bbeb05efa543e5a708c56e3e648e794fcc139
Patrick Uiterwijk <puiterwijk@gmail.com>
2012-10-30 16:06:42 +0100

    Let admin use the configuration system

--- modules/admin/admin.c
+++ modules/admin/admin.c
@@ -11,6 +11,10 @@
 #include <utils.h>
 #include <web_module.h>
 
+#include <services/Configuration.h>
+
+ServiceInfoConfiguration *config;
+
 int get_interface_version()
 {
     return WEB_MODULE_INTERFACE_VERSION;
@@ -71,6 +75,14 @@ SoupAuthDomain *admin_domain = NULL;
 
 bool install(WebServer *server)
 {
+    ServiceInfo *configInfo = server->get_system_service(SERVICE_TYPE_CONFIGURATION);
+    if(configInfo == NULL)
+    {
+        DEBUG("Configuration system service is not provided, admin module cannot initialize", NULL);
+        return FALSE;
+    }
+    config = (ServiceInfoConfiguration*)configInfo->get_service();
+
     admin_domain = soup_auth_domain_digest_new(SOUP_AUTH_DOMAIN_REALM, "MyWebsite Admin",
                                                SOUP_AUTH_DOMAIN_DIGEST_AUTH_CALLBACK, admin_auth_callback,
                                                SOUP_AUTH_DOMAIN_DIGEST_AUTH_DATA, NULL,
